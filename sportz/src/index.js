8dd5275e91564aef346800f8bafa1bb45810c2d6
rajpratap29 <therajpratapofficial@gmail.com>
2026-02-07 16:07:17 +0530

    implement matches POST and GET routes

--- sportz/src/index.js
+++ sportz/src/index.js
@@ -1,13 +1,16 @@
-import express from 'express';
+import express from "express";
+import { matchRouter } from "./routes/matches.js";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express Server!');
+app.get("/", (req, res) => {
+  res.send("Hello from Express Server!");
 });
+
+app.use("/matches", matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
